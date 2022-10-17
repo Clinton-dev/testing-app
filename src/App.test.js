258491bc66bda9588631b70c430bae81bb7e4644
Clinton wambugu <clintonwambugu@gmail.com>
2022-10-17 23:12:08 +0300

    Add test to app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    const { getByRole } = render(<App />)
+    expect(getByRole("heading").textContent).toMatch(/our first test/i)
+  })
 });
+
+describe("App Component", () => {
+  it("renders magnificent monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot()
+  });
+
+  it("renders radical rhinos after button click", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: "click Me" });
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhino/i)
+  })
+})
+
+// describe("App component", () => {
+//   it("renders magnificent monkeys", () => {
+//     // since screen does not have the container property, we'll destructure render to obtain container for this test
+//     const { container } = render(<App />);
+//     expect(container).toMatchSnapshot();
+//   });
+
+//   it("renders radical rhinos after button click", () => {
+//     render(<App />);
+//     const button = screen.getByRole("button", { name: "Click Me" });
+
+//     userEvent.click(button);
+
+//     expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+//   });
+// });
